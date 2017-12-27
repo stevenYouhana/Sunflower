6909b727ab1260b78fc175d0d24605af07e210f5
Steven Youhana <stevenyouhana@hotmail.co.nz>
2017-12-27 19:50:28 +1300

    removed unwanted files

--- TestClass.hpp
+++
@@ -1,9 +0,0 @@
-#include <StandardCplusplus.h>
-#include <string>
-using std::string;
-
-class TestClass {
-  public:
-    string displayText(){return "displaying testClass   ";}
-    int showNum(){return 7;}
-};
